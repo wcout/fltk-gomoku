d09438ce37b0d255e4da2372c647039f7771f92e
wcout <wcout@users.noreply.github.com>
2017-11-05 09:23:43 +0100

    +first outline

---
+++ fltk-gomoku.cxx
@@ -0,0 +1,138 @@
+/*
+	FLTK Gomoku (c) 2017
+*/
+#include <FL/Fl.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/fl_draw.H>
+#include <cstdlib>
+
+#define FL_DARK_GRAY fl_darker( FL_GRAY )
+
+class Board : public Fl_Double_Window
+{
+typedef Fl_Double_Window Inherited;
+public:
+	Board() :
+		Inherited( 600, 600, "FLTK Gomoku" ),
+		_G( 18 ),
+		_player( true ),
+		_pondering( false )
+	{
+		memset( _board, 0, sizeof( _board ) );
+		resizable( this );
+	}
+	int xp( int x_ ) const
+	{
+		return w() / ( _G + 2 ) * x_;
+	}
+	int yp( int y_ ) const
+	{
+		return h() / ( _G + 2 ) * y_;
+	}
+	void draw_piece( int color_, int x_, int y_ ) const
+	{
+		int x = xp( x_ );
+		int y = yp( y_ );
+		int rw = xp( 1 ) - 2;
+		int rh = yp( 1 ) - 2;
+		fl_color( color_ == 1 ? FL_WHITE : FL_BLACK );
+		fl_pie( x - rw/2, y - rh/2, rw, rh, 0, 360 );
+		fl_color( FL_GRAY );
+		fl_arc( x - rw/2, y - rh/2, rw, rh, 0, 360 );
+		fl_color( fl_darker( FL_GRAY ) );
+		fl_arc( x - rw/2 + 1, y - rh/2 + 1, rw - 2, rh - 2, 0, 360 );
+	}
+	void onMove()
+	{
+		// stub
+		int G = _G / 2;
+		while ( 1 )
+		{
+			int x = random() % ( G + 1 ) + G / 2 + 1;
+			int y = random() % ( G + 1 ) + G / 2 + 1;
+			if ( _board[x][y] ) continue;
+			setPiece( x, y, 1 );
+			break;
+		}
+	}
+	static void cb_move( void *d_ )
+	{
+		(static_cast<Board *>(d_))->pondering( false );
+	}
+	void makeMove()
+	{
+		_pondering = true;
+		fl_cursor( FL_CURSOR_WAIT );
+		Fl::add_timeout( 1.0, cb_move, this );
+		while ( _pondering )
+		{
+			Fl::check();
+		}
+		fl_cursor( FL_CURSOR_DEFAULT );
+		onMove();
+	}
+	void setPiece( int x_, int y_, int color_ )
+	{
+		if ( x_ < 1 || x_ > _G + 1 )
+			return;
+		if ( y_ < 1 || y_ > _G + 1 )
+			return;
+		_board[ x_][ y_ ] = color_ + 1;
+		redraw();
+		_player = !_player;
+		if ( !_player )
+		{
+			makeMove();
+
+		}
+	}
+	int handle( int e_ )
+	{
+		if ( e_ == FL_PUSH && _player )
+		{
+			int x = ( Fl::event_x() + xp( 1 ) / 2 ) / xp( 1 );
+			int y = ( Fl::event_y() + yp( 1 ) / 2 ) / yp( 1 );
+			setPiece( x, y, 0 );
+			return 1;
+		}
+		return Inherited::handle( e_ );
+	}
+private:
+	virtual void draw()
+	{
+		// draw board
+		fl_rectf( 0, 0, w(), h(), FL_DARK_GRAY );
+		fl_rectf( 0, 0, xp(_G+2), yp(_G+2), fl_lighter( FL_YELLOW ) );
+
+		// draw grid
+		fl_rect( 0, 0, xp(_G+2), yp(_G+2), FL_DARK_GRAY );
+		for ( int x = 0; x <= _G; x++ )
+			fl_line( xp(x + 1 ), yp( 1 ), xp( x + 1 ), yp( 1  + _G ) );
+
+		for ( int y = 0; y <= _G; y++ )
+			fl_line( xp( 1 ), yp( y + 1), xp( 1 + _G ), yp( y + 1 ) );
+
+		// draw center
+		fl_color( FL_BLACK );
+		fl_circle( xp( _G / 2 + 1 ), yp( _G / 2 + 1), _G / 6 );
+
+		// draw pieces
+		for ( int x = 1; x <= _G + 1; x++ )
+			for ( int y = 1; y <= _G + 1; y++ )
+				if ( _board[x][y] )
+					draw_piece( _board[x][y], x, y );
+	}
+	void pondering( bool pondering_ ) { _pondering = pondering_; }
+private:
+	int _G;
+	int _board[30][30];
+	bool _player;
+	bool _pondering;
+};
+
+int main()
+{
+	Board g;
+	g.show();
+	return Fl::run();
+}
