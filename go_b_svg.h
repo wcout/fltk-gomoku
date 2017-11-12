663944715849cf9376eb22c09093039b7706ba96
wcout <wcout@users.noreply.github.com>
2017-11-12 17:14:37 +0100

    +SVG pieces; show game statistics

---
+++ go_b_svg.h
@@ -0,0 +1,11 @@
+const char *Go_Black_Piece =
+	"<?xml version=\"1.0\"?>\n"
+	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"500\">\n"
+	"<defs><radialGradient id=\"rg\" cx=\"30%\" cy=\"30%\" r=\"80%\">\n"
+	"<stop offset=\"0%\" stop-color=\"#777777\"/>\n"
+	"<stop offset=\"30%\" stop-color=\"#222222\"/>\n"
+	"<stop offset=\"100%\" stop-color=\"#000000\"/>\n"
+	"</radialGradient></defs>\n"
+	"<rect width=\"500\" height=\"500\" fill=\"none\"/>\n"
+	"<circle cx=\"250\" cy=\"250\" r=\"245\" fill=\"url(#rg)\"/>\n"
+	"</svg>";
---
+++ go_w_svg.h
@@ -0,0 +1,11 @@
+const char *Go_White_Piece =
+	"<?xml version=\"1.0\"?>\n"
+	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"500\">\n"
+	"<defs><radialGradient id=\"rg\" cx=\"47%\" cy=\"49%\" r=\"48%\">\n"
+	"<stop offset=\"70%\" stop-color=\"#FFFFFF\"/>\n"
+	"<stop offset=\"90%\" stop-color=\"#DDDDDD\"/>\n"
+	"<stop offset=\"100%\" stop-color=\"#777777\"/>\n"
+	"</radialGradient></defs>\n"
+	"<rect width=\"500\" height=\"500\" fill=\"none\"/>\n"
+	"<circle cx=\"250\" cy=\"250\" r=\"245\" fill=\"url(#rg)\"/>\n"
+	"</svg>\n";
