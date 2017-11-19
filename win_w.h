b9e61d3f19679e589f428d1f5fe9e17ed301f8c3
wcout <wcout@users.noreply.github.com>
2017-11-19 13:00:00 +0100

    ~FL_GREEN ~ 'lime' in SVG

--- win_w.h
+++ win_w.h
@@ -1,5 +1,5 @@
 const char *Win_White_Piece =
 	"<?xml version=\"1.0\"?>\n"
 	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"500\">\n"
-	"<circle cx=\"250\" cy=\"250\" r=\"235\" stroke=\"green\" stroke-width=\"30\" fill=\"none\"/>\n"
+	"<circle cx=\"250\" cy=\"250\" r=\"235\" stroke=\"lime\" stroke-width=\"30\" fill=\"none\"/>\n"
 	"</svg>\n";
