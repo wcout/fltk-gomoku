b1daf3d99f5e3e4d03fe2e66a37f9f482fe67bca
wcout <wcout@users.noreply.github.com>
2017-11-19 10:08:31 +0100

    +enlarge outline width of highlighted pieces

--- last_piece.h
+++ last_piece.h
@@ -1,5 +1,5 @@
 const char *Last_Piece =
 	"<?xml version=\"1.0\"?>\n"
 	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"500\">\n"
-	"<circle cx=\"250\" cy=\"250\" r=\"240\" stroke=\"cyan\" stroke-width=\"20\" fill=\"none\"/>\n"
+	"<circle cx=\"250\" cy=\"250\" r=\"235\" stroke=\"cyan\" stroke-width=\"30\" fill=\"none\"/>\n"
 	"</svg>\n";
