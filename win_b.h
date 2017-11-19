f018d9a089402d109ec9551989775146e4ca8e6d
wcout <wcout@users.noreply.github.com>
2017-11-19 10:02:44 +0100

    +draw highlighted pieces from svg images

---
+++ win_b.h
@@ -0,0 +1,5 @@
+const char *Win_Black_Piece =
+	"<?xml version=\"1.0\"?>\n"
+	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"500\">\n"
+	"<circle cx=\"250\" cy=\"250\" r=\"240\" stroke=\"red\" stroke-width=\"20\" fill=\"none\"/>\n"
+	"</svg>\n";
