dce3e4d8decd8019e3adbcad0d53be36161d8c8e
wcout <wcout@users.noreply.github.com>
2026-02-04 10:52:23 +0100

    ~update build script

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # If using a not installed FLTK version, specify it's path here (with '/' at end)
-#FLTK=../fltk-1.4/
+#FLTK=../fltk/build/
 FLTK_CONFIG="$FLTK"fltk-config
 
 TARGET=fltk-gomoku
 SRC=fltk-gomoku.cxx
-OPT=-DFLTK_USE_NANOSVG
+if [ -f miniaudio.h ]; then
+OPT=-DUSE_MINIAUDIO
+fi
 
 g++ -Wall $OPT -o $TARGET `$FLTK_CONFIG --use-images --cxxflags` $SRC `$FLTK_CONFIG --use-images --ldflags`
